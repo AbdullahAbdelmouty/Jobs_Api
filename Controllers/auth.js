8019b1f67de415ed87416ba7401102022ff9d818
Abdullah <abdallah0115019@gmail.com>
2023-07-08 05:22:50 +0300

    job model

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,5 +1,7 @@
+const User  = require("../Models/User");
 const login = async(req,res)=>{
-    res.status(200).send("login");
+    const user = await User.create({...req.body});
+    res.status(200).json({user});
 }
 
 const register = async(req,res)=>{
