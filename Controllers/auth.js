0304ded4cc3e5842a5e1b42cb2a8bde15c94e91b
Abdullah <abdallah0115019@gmail.com>
2023-07-08 05:23:48 +0300

    register controler

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,10 +1,10 @@
 const User  = require("../Models/User");
-const login = async(req,res)=>{
+const register = async(req,res)=>{
     const user = await User.create({...req.body});
     res.status(200).json({user});
 }
 
-const register = async(req,res)=>{
+const login = async(req,res)=>{
     res.status(200).send("register");
 }
 
