f351aef750b13dcd2679b686289401e972866c95
Abdullah <abdallah0115019@gmail.com>
2023-07-08 05:53:37 +0300

    hashing password using bcryptjs

--- Models/User.js
+++ Models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcryptjs");
 const UserSchema = new mongoose.Schema({
 name: {
     type: String,
@@ -21,6 +21,12 @@ name: {
     required: [true, 'Please provide password'],
     minlength: 6,
     },
+})
+
+UserSchema.pre("save",async function(){
+    const salt = await bcrypt.genSalt(10);
+    this.password = bcrypt.hash(this.password,salt);
+
 })
 
 module.exports = mongoose.model("User",UserSchema)
