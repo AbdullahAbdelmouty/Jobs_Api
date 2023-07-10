86000de5a44b43c2125cfc6368ea938140c55b18
Abdullah <abdallah0115019@gmail.com>
2023-07-10 16:17:16 +0300

    updateJob controller

--- Controllers/jobs.js
+++ Controllers/jobs.js
@@ -1,5 +1,5 @@
 const Job = require('../Models/Job');
-const {NotFoundError} = require('../Errors')
+const {NotFoundError, BadRequestError} = require('../Errors')
 const getAllJobs = async(req,res)=>{
     const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt');
     res.status(200).json({jobs});
@@ -18,10 +18,31 @@ const createJob = async(req,res)=>{
     res.status(201).json({job});
 }
 const deleteJob = async(req,res)=>{
-    
+    const {user:{userId},params:{id:jobId}} = req;
+    const job = await Job.findByIdAndRemove({_id:jobId,createdBy:userId});
+    if(!job){
+        throw new NotFoundError(`job by id ${jobId} not exist`);
+    }
+    res.status(200).send('deleted');
 }
 const updateJob = async(req,res)=>{
-    res.status(200).send("update job");
+    const {
+        body: { company, position },
+        user:{userId},
+        params:{id:jobId}
+        } = req;
+        if(!company||!position){
+            throw new BadRequestError('Company or Position fields cannot be empty');
+        }
+        const job = await Job.findOneAndUpdate({
+        _id:jobId,createdBy:userId},
+        req.body,{new:true,runValidators:true});
+
+
+    if(!job){
+        throw new NotFoundError(`job by id ${jobId} not exist`);
+    }
+    res.status(200).json({job});
 }
 
 module.exports = {
